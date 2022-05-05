9e7eff8a0f038c64ba46e4caa8bc377055af93dd
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-05 19:34:54 -0300

    feat: header navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { 
+  Header,
+} from './components'
+export const App = () => {
+  return (
+    <>
+      <main className='app'>
+      </main>
+    </>
+  )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss'
+
+export const Header = () => {
+  return (
+    <>
+      <header className='header'>
+        <nav className='nav bg-grid'>
+          <div className="nav__logo">
+            <a href="#" >EduardoSilva</a>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
