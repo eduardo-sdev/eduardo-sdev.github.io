0203316c7299d51b01481298087e663c76febf8a
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-08 13:42:55 -0300

    style: section project span bold

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,71 +1,71 @@
 import {
-  CalendarCheck,
-  HourglassHigh,
-  Terminal,
-  CheckSquare,
-  Rocket,
-  FlagBanner,
-  Stack
-} from 'phosphor-react'
+    CalendarCheck,
+    HourglassHigh,
+    Terminal,
+    CheckSquare,
+    Rocket,
+    FlagBanner,
+    Stack,
+} from "phosphor-react";
 
-import './style.scss'
+import "./style.scss";
 
-export const Project = ({project , id}) => {
-  return (
-    <div key={id} className="project__data">
-      <div className="project__header">
+export const Project = ({ project, id }) => {
+    return (
+        <div key={id} className="project__data">
+            <div className="project__header">
+                <div className="box">
+                    <div className="project__image">
+                        <img src={project.imgurl} alt="" />
+                    </div>
+                    <div>
+                        <div className="project__title">{project.title}</div>
 
-        <div className='box'>
-          <div className="project__image">
-            <img src={project.imgurl} alt="" />
-          </div>
-          <div>
-            <div className="project__title">
-              {project.title}
+                        <div className="project__subtitle">
+                            {project.subtitle}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="project__date">
+                    <CalendarCheck />: {project.date}
+                </div>
             </div>
 
-            <div className="project__subtitle">
-              {project.subtitle}
-            </div>
-          </div>
+            <ul className="project__list">
+                <li className="project__list-item">
+                    <HourglassHigh />
+                    <span>Status: </span>
+                    {project.status}
+                </li>
+
+                <li className="project__list-item">
+                    <CheckSquare />
+                    <span>Task: </span>
+                    {project.task}
+                </li>
+
+                <li className="project__list-item">
+                    <FlagBanner />
+                    <span>Skills: </span>
+                    {project.skills}
+                </li>
+                <li className="project__list-item">
+                    <Stack />
+                    <span>Stacks: </span>
+                    {project.stacks}
+                </li>
+                <li className="project__list-item">
+                    <Rocket />
+                    <span>Deploy: </span>
+                    <a href={project.deploy}>Deploy</a>
+                </li>
+                <li className="project__list-item">
+                    <Terminal />
+                    <span>Código: </span>
+                    <a href={project.repo}>Ver Código</a>
+                </li>
+            </ul>
         </div>
-
-        <div className="project__date">
-          <CalendarCheck />
-          : {project.date}
-        </div>
-
-      </div>
-      
-      <div className="project__status">
-        <HourglassHigh />
-        : {project.status}
-      </div>
-
-      <div className="project__task">
-        <CheckSquare />
-        : {project.task}
-      </div>
-
-      <div className="project__skills">
-        <FlagBanner />
-        : {project.skills}
-      </div>
-      <div className="project__stacks">
-        <Stack />
-        : {project.stacks}
-      </div>
-
-      <div className="project__deploy">
-        <Rocket />
-        : <a href={project.deploy}>Deploy</a>
-      </div>
-
-      <div className="project__deploy">
-        <Terminal />
-        : <a href={project.repo}>Ver Código</a>
-      </div>
-
-    </div>
-  )
-}
+    );
+};
