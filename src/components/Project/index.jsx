85b2cd0c749fa3df63c83d505f7e022b868a4e2e
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-28 22:05:09 -0300

    feat: project in datocms

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -14,64 +14,67 @@ import "./style.scss";
 
 export const Project = ({ project }) => {
     return (
-        <div key={uniqid()} className="project__data">
-            <div className="project__header">
-                <div className="box">
-                    <div className="project__image">
-                        <img src={project.imgurl} alt="" />
-                    </div>
-                    <div>
-                        <div className="project__title">{project.title}</div>
+         <div key={uniqid()} className="project__data">
+             <div className="project__header">
+                 <div className="box">
+                     <div className="project__image">
+                        <img src={project.imageurl.url} alt="" />
+                     </div>
+                     <div>
+                         <div className="project__title">{project.title}</div>
 
-                        <div className="project__subtitle">
-                            {project.subtitle}
-                        </div>
-                    </div>
-                </div>
+                         <div className="project__subtitle">
+                             {project.subtitle}
+                         </div>
+                     </div>
+                 </div>
 
-                <div className="project__date">
-                    <CalendarCheck />: {project.date}
-                </div>
-            </div>
+                 <div className="project__date">
+                     <CalendarCheck />: {project.data}
+                 </div>
+             </div>
 
-            <ul className="project__list">
-                <li className="project__list-item">
-                    <HourglassHigh />
-                    <span>Status: </span>
-                    {project.status}
-                </li>
+             <ul className="project__list">
+                 <li className="project__list-item">
+                     <HourglassHigh />
 
-                <li className="project__list-item">
-                    <CheckSquare />
-                    <span>Task: </span>
-                    {project.task}
-                </li>
-
-                <li className="project__list-item">
-                    <FlagBanner />
-                    <span>Skills: </span>
-                    {project.skills}
-                </li>
-                <li className="project__list-item">
-                    <Stack />
-                    <span>Stacks: </span>
-                    <ul className="stack__list">
-                        {project.stacks.map(stak => (
-                            <li className="stack__list-item" key={uniqid()}>{stak}</li>
-                        ))}
-                    </ul>
-                </li>
-                <li className="project__list-item">
-                    <Rocket />
-                    <span >Deploy: </span>
-                    <a className="link" href={project.deploy}>Deploy</a>
-                </li>
-                <li className="project__list-item">
-                    <Terminal />
-                    <span>Código: </span>
-                    <a className="link" href={project.repo}>Ver Código</a>
-                </li>
-            </ul>
-        </div>
+                     <span>Status: </span>
+                     {project.statusdev}
+                 </li>
+                 <li className="project__list-item">
+                     <CheckSquare />
+                     <span>task: </span>
+                     {project.task}
+                 </li>
+                 <li className="project__list-item">
+                     <FlagBanner />
+                     <span>Skills: </span>
+                     {project.skill}
+                 </li>
+                 <li className="project__list-item">
+                     <Stack />
+                     <span>Stacks: </span>
+                     <ul className="stack__list">
+                         {project.stacks.map(stak => (
+                             <li className="stack__list-item" key={uniqid()}>{stak}</li>
+                         ))}
+                     </ul>
+                 </li>
+                     {project.deploy &&
+                         <li className="project__list-item">
+                             <Rocket />
+                             <span >Deploy: </span>
+                             <a target="_blank" rel="noopener noreferrer"
+                             className="link" href={project.deploy}>Deploy</a>
+                         </li>}
+                 <li className="project__list-item">
+                     <Terminal />
+                     <span>Código: </span>
+                     <a target="_blank" rel="noopener noreferrer"
+                     className="link" href={project.repo}>Ver Código</a>
+                 </li>
+             </ul>
+         </div>
     );
 };
+
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,20 +1,58 @@
-import { db } from './db'
+import { useEffect, useState } from 'react'
 
 import uniqid from 'uniqid'
 
-import {Project} from '../Project' 
+import {Project} from '../Project'
 
 export const Projects = () => {
+    const [projects, setProjects] = useState()
+
+    useEffect(() => {
+        fetch('https://graphql.datocms.com/', {
+          method: 'POST',
+          headers: {
+            'Authorization': process.env.REACT_APP_DATO_API_TOKEN,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify({ "query": `query {
+            allProjects {
+                title
+                id
+                data
+                statusdev
+                task
+                stacks
+                skill
+                deploy
+                repo
+                imageurl {
+                  url
+                }
+              }
+            }
+          ` })
+        })
+      .then((resp) => resp.json())
+      .then((respComp) => {
+
+        const all_projects = respComp.data.allProjects;
+          setProjects(all_projects);
+      });
+    },[])
+
   return (
     <>
       <section className="projects">
         <div className="projects__container">
           {
-            db.map(proj => {
-              return (
-                <Project key={uniqid()} project={proj} />
-              )
-            })
+            projects ? (
+             projects.map(proj => {
+               return (
+                 <Project key={uniqid()} project={proj} />
+               )
+             })
+            ) : (<h3>Carregando Projetos...</h3>)
           }
         </div>
       </section>
